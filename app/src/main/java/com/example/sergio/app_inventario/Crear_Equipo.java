7d971ab320a010c1afca253c5221f4a8583fedfc
Sergio Ramirez Dominguez <Sergio@MacBook-Air-de-Sergio.local>
2016-07-06 20:46:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sergio/app_inventario/Crear_Equipo.java
@@ -0,0 +1,18 @@
+package com.example.sergio.app_inventario;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sergio on 22/6/16.
+ */
+public class Crear_Equipo extends Activity {
+
+    @Override public void onCreate(Bundle saveInstanceState){
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.crear_equipo);
+
+    }
+
+}
